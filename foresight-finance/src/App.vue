0596d76be41a243f516765d0b1560d58f68988b6
st8farmfromjake <jake.shumate33823@gmail.com>
2024-09-06 13:43:20 -0400

    Added background (getting darker/lighter) along with navbar.

--- foresight-finance/src/App.vue
+++ foresight-finance/src/App.vue
@@ -1,30 +1,68 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link id="FFLogo" to="/">Foresight Finance </router-link>
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+html, body{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Monda", sans-serif;
+  
 }
 
 nav {
   padding: 30px;
+  background-color: #183243;
+  position:fixed; 
+  width:100vw;
+  
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #e3b130;
+  text-decoration: none;
+}
+
+#FFLogo{
+  font-size: 2vw;
+  text-transform: uppercase;
+  line-height:1;
+  text-align: center;
+  background: linear-gradient(90deg, rgba(186,148,62,1) 0%, rgba(236,172,32,1) 20%, rgba(186,148,62,1) 39%, rgba(249,244,180,1) 50%, rgba(186,148,62,1) 60%, rgba(236,172,32,1) 80%, rgba(186,148,62,1) 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;	
+  animation: shine 3s infinite ease-in-out;
+  background-size: 200%;
+  background-position: left;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  /*Will have the actice nav bar links be animated gold when active */
+  font-size: 2vw;
+  text-transform: uppercase;
+  line-height:1;
+  text-align: center;
+  background: linear-gradient(90deg, rgba(186,148,62,1) 0%, rgba(236,172,32,1) 20%, rgba(186,148,62,1) 39%, rgba(249,244,180,1) 50%, rgba(186,148,62,1) 60%, rgba(236,172,32,1) 80%, rgba(186,148,62,1) 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;	
+  animation: shine 3s infinite ease-in-out;
+  background-size: 200%;
+  background-position: left;
+}
+
+@keyframes shine {
+  from{background-position: -10%;}
+  to{background-position: 110%}
+ 
 }
 </style>
--- foresight-finance/src/views/HomeView.vue
+++ foresight-finance/src/views/HomeView.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="zoom-background">
+    <h2 id="FFLogoHeader">Foresight Finance</h2>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+window.addEventListener('DOMContentLoaded', () => {
+  //will run scripts on zoom background
+  let box = document.querySelector(".zoom-background");
+  let images = [require('@/assets/marbleHOMEPAGE-zoom-0-50-Darker.jpg'), require('@/assets/marbleHOMEPAGE-zoom-0.jpg')]
+  //^^list of images
+  window.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY;
+    
+    let viewportHeight = window.innerHeight;
+    let imageIndex = Math.floor(scrollPosition / viewportHeight % images.length);
+    box.style.backgroundImage = `url(${images[imageIndex]})`;
+  })
+
+})
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 600vh;
+}
+
+.zoom-background {
+  position: fixed;
+  margin-top: 9vh;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: url(../assets/marbleHOMEPAGE-zoom-0-50-Darker.jpg);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-image 1.5s;
+}
+
+#FFLogoHeader {
+  color: white;
+}
+</style>
