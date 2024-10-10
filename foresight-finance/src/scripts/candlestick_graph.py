115448d23ad555e54e8f5167facc908270189b0e
Ezra Cooper <EzraCooper@outlook.com>
2024-10-10 13:00:09 -0400

    Retrieved all of the relevant stock data and displayed it for the user's convenience; Pulled the list of stocks from the .txt file instead of it being hard coded

--- foresight-finance/src/scripts/candlestick_graph.py
+++ foresight-finance/src/scripts/candlestick_graph.py
@@ -3,18 +3,17 @@ import plotly.graph_objects as go
 import os
 from flask import Flask, request, jsonify, send_from_directory
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# Function to filter data based on trading hours (9:30 AM to 4:00 PM, Monday to Friday)
-def filter_trading_hours(df):
-    # Filter out weekends (Monday=0, Sunday=6)
-    df = df[df.index.weekday < 5]
-    
-    # Filter out times outside of trading hours (9:30 AM to 4:00 PM)
-    df = df.between_time('09:30', '16:00')
-    
-    return df
+@app.route('/')
+def homepage():
+    return '''
+    <h1>Welcome to the Stock Data App</h1>
+    <p>Use <a href="/generate_chart?ticker=NVDA">/generate_chart</a> to generate a candlestick chart.</p>
+    '''
 
 @app.route('/generate_chart', methods=['GET'])
 def generate_chart():
@@ -70,48 +69,71 @@ def generate_chart():
     # Get additional stock information
     try:
         info = yf.Ticker(ticker).info
-        volume = f"{data['Volume'].sum():,}"
         
-        # Check if market_cap is available and is a number
+        # Calculate total volume
+        volume = f"{data['Volume'].sum():,}"
+
+        # Ensure market_cap is formatted correctly
         market_cap = info.get('marketCap')
         if market_cap is not None:
             market_cap = f"{market_cap:,}"  # Format as string with commas
         else:
             market_cap = 'N/A'
-        
-        # Check if regularMarketChangePercent is available and is a number
-        percent_change = info.get('regularMarketChangePercent')
-        if percent_change is not None:
-            percent_change = f"{percent_change:.2f}%"  # Format as percentage
+
+        # Calculate percent change based on the last closing price and the first closing price
+        if not data.empty:
+            last_close = data['Close'].iloc[-1]
+            first_close = data['Close'].iloc[0]
+
+            if first_close > 0:
+                percent_change = ((last_close - first_close) / first_close) * 100
+                percent_change = f"{percent_change:.2f}%"  # Format as percentage
+            else:
+                percent_change = 'N/A'  # If the opening price is zero or no data
         else:
             percent_change = 'N/A'
 
         return jsonify({
             "success": True,
-            "stockName": info.get('longName', ticker),  # Use longName if available
-            "stockCode": ticker,
+            "longName": info.get('longName', ticker),
+            "symbol": info.get('symbol', ticker),
             "volume": volume,
             "marketCap": market_cap,
             "percentChange": percent_change,
+            "currentPrice": info.get('currentPrice', 'N/A'),
+            "sector": info.get('sector', 'N/A'),
+            "industry": info.get('industry', 'N/A'),
         })
 
     except Exception as e:
         print(f"Error fetching stock info: {e}")
         return jsonify({
-            "success": True,
-            "stockName": ticker,
-            "volume": volume,
+            "success": False,
+            "longName": 'N/A',
+            "symbol": ticker,
+            "volume": 'N/A',
             "marketCap": 'N/A',
             "percentChange": 'N/A',
         })
 
 # Route to serve the stock_data.txt file
-@app.route('/static/stock_data.txt', methods=['GET'])
+@app.route('/src/static/stock_data.txt', methods=['GET'])
 def serve_stock_data():
     try:
         return send_from_directory('static', 'stock_data.txt')
     except Exception as e:
         return jsonify({"success": False, "message": "File not found."}), 404
+
+# Function to filter data based on trading hours (9:30 AM to 4:00 PM, Monday to Friday)
+def filter_trading_hours(df):
+    # Filter out weekends (Monday=0, Sunday=6)
+    
+    df = df[df.index.weekday < 5]
+    
+    # Filter out times outside of trading hours (9:30 AM to 4:00 PM)
+    df = df.between_time('09:30', '16:00')
+    
+    return df
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- foresight-finance/src/views/AIPatternRecognizerView.vue
+++ foresight-finance/src/views/AIPatternRecognizerView.vue
@@ -26,11 +26,17 @@
 
       <!-- Stock information -->
       <div class="stock-info">
-        <p style="color: white;">Symbol: <span>{{ stockCode }}</span></p>
-        <p style="color: white;">Company Name: <span>{{ stockName }}</span></p>
-        <p style="color: white;">Market Cap: <span>{{ marketCap }}</span></p>
-        <p style="color: white;">% Change: <span>{{ percentChange }}</span></p>
-        <p style="color: white;">Volume: <span>{{ volume }}</span></p>
+        <div style="display: grid; grid-template-columns: repeat(4, 2fr); gap: 10px; margin-left: -400px;">
+          <p style="color: white;">Symbol: <span>{{ stockCode }}</span></p>
+          <p style="color: white;">Company Name: <span>{{ stockName }}</span></p>
+          <p style="color: white;">Current Price: <span>{{ currentPrice }}</span></p>
+          <p style="color: white;">Market Cap: <span>{{ marketCap }}</span></p>
+
+          <p style="color: white;">% Change: <span>{{ percentChange }}</span></p>
+          <p style="color: white;">Volume: <span>{{ volume }}</span></p>
+          <p style="color: white;">Industry: <span>{{ industry }}</span></p>
+          <p style="color: white;">Sector: <span>{{ sector }}</span></p>
+      </div>
       </div>
     </div>
 
@@ -59,63 +65,72 @@ export default {
     return {
       ticker: '',
       chartUrl: this.getChartUrl(''),
-      stockName: '...',
-      stockCode: '...',
-      marketCap: '...',
-      percentChange: '...',
-      volume: '...',
       loading: false,
       refreshing: false,
       countdown: 120,
       refreshInterval: null,
       countdownInterval: null,
-      isModalOpen: false, // Control the modal state
+      isModalOpen: false,
       stockData: '', // Holds stock data from the file
-      // Hardcoded list of stock symbols
-      stockSymbols: [
-        'AAPL - Apple Inc.', 'GOOGL - Alphabet Inc.',  'MSFT - Microsoft Corp.',  'AMZN - Amazon.com Inc.',  'TSLA - Tesla Inc.',  'FB - Meta Platforms Inc.',  'NFLX - Netflix Inc.',  'NVDA - NVIDIA Corp.',  'BRK.B - Berkshire Hathaway Inc.',  'JPM - JPMorgan Chase & Co.',  'ABT - Abbott Laboratories',  'ANF - Abercrombie & Fitch Co.',  'ABGX - Abgenix Inc.',  'ADPT - Adaptec Inc',  'ADBE - Adobe Systems Inc.',  'ADIC - Advanced Digital Information',  'AMD - Advanced Micro Devices',  'ADVS - Advent Software Inc.',  'ARXX - Aeroflex Inc.',  'AES - AES Corp',  'AET - Aetna Inc.',  'AFFX - Affymetrix Inc.',  'AFL - AFLAC Inc',  'A - Agilent Technologies Inc.',  'APD - Air Products & Chem',  'AKAM - Akamai Technologies Inc.',  'ACV - Alberto-Culver Cl\'B\'',  'ABS - Albertson\'s, Inc',  'AL - Alcan Aluminium Ltd',  'AA - Alcoa Inc.',  'ATI - Allegheny Technologies',  'AGN - Allergan, Inc',  'AW - Allied Waste Ind',  'ALL - Allstate Corp',  'AT - Alltel Corp',  'ALTR - Altera Corp',  'AMZN - Amazon.com Inc.',  'AEP - Amer Electric Pwr',  'AM - Amer Greetings Cl\'A\'',  'AIG - Amer Intl Group',  'APCC - Amer Power Conversion',  'AHC - Amerada Hess',  'AEE - Ameren Corp',  'AEOS - American Eagle Outfitters Inc.',  'AXP - American Express Company',  'AMTD - Ameritrade Holding Corp.',  'AMGN - Amgen Inc',  'AMR - AMR Corp',  'ASO - AmSouth Bancorp',  'APC - Anadarko Petroleum',  'ADI - Analog Devices Inc.',  'ANDW - Andrew Corp',  'ADRX - Andrx Corporation',  'BUD - Anheuser-Busch Cos',  'TWX - AOL Time Warner',  'AOC - Aon Corp',  'APA - Apache Corp',  'AAPL - Apple Computer Inc.',  'APPB - Applebees International Inc.',  'AMAT - Applied Materials Inc.',  'AMCC - Applied Micro Circuits Corp',  'ADM - Archer-Daniels-Midland',  'ARIA - Ariad Pharmaceuticals Inc.',  'ARBA - Ariba Inc.',  'ARW - Arrow Electronics Inc.',  'ASH - Ashland Inc',  'ASKJ - Ask Jeeves Inc.',  'T - AT&T Corp',  'AWE - AT&T Wireless Corp',  'ADSK - Autodesk, Inc',  'ADP - Automatic Data Proc',  'AZO - AutoZone Inc',  'AVY - Avery Dennison Corp',  'AVA - Avista Corp',  'AVP - Avon Products Inc.',  'BHI - Baker Hughes Inc',  'BLL - Ball Corp',  'BAC - Bank of America',  'BK - Bank of New York Inc.',  'ONE - Bank One Corp',  'BCR - Bard (C.R.)',  'BKS - Barnes & Noble Inc.', 'ABX - Barrick Gold',  'BOL - Bausch & Lomb Inc.',  'BAX - Baxter International',  'BBT - BB&T Corp',  'BCE - BCE Inc.',  'BEAS - BEA Systems Inc.',  'BSC - Bear Stearns Cos',  'BDX - Becton, Dickinson',  'BBBY - Bed Bath & Beyond Inc.',  'BLS - BellSouth Corp',  'BMS - Bemis Co',  'BBY - Best Buy Co. Inc',  'BLI - Big Lots Inc.',  'BMET - Biomet, Inc',  'BBH - Biotch Holders Trust',  'BDK - Black & Decker Corp',  'HRB - Block H & R Inc.',  'BBI - Blockbuster Inc.',  'BMC - BMC Software',  'BA - Boeing Co.',  'BCC - Boise Cascade',  'BSX - Boston Scientific',  'BP - BP Plc',  'BGG - Briggs & Stratton',  'BMY - Bristol-Myers Squibb',  'BSY - British Sky Broadcasting',  'BTY - British Telecommunications',  'BRCM - Broadcom Corp\'A\'',  'BRCD - Brocade Comm. Systems',  'BC - Brunswick Corp',  'BNI - Burlington Northn Santa Fe',  'BR - Burlington Resources',  'CPN - Calpine Corp',  'CPB - Campbell Soup Co.',  'COF - Capital One Financial',  'CAH - Cardinal Health',  'CCL - Carnival Corp',  'CAT - Caterpillar Inc.',  'CRA - Celera Genomics',  'CELG - Celgene Corporation',  'CD - Cendant Corp',  'CTX - Centex Corp',  'CTL - CenturyTel Inc',  'CEN - Ceridian Corp',  'CF - Charter One Finl',  'CHKP - Checkpoint Software',  'CVX - ChevronTexaco Corp',  'CHL - China Telecommunications',  'CHINA - China.com Corp',  'CHIR - Chiron Corp',  'CB - Chubb Corp',  'CI - CIGNA Corp',  'CINF - Cincinnati Financial',  'CIN - CINergy Corp',  'CC - Circuit City Strs-CrctCtyGrp',  'CSCO - Cisco Systems Inc.',  'C - Citigroup Inc.',  'CTXS - Citrix Systems Inc.',  'CCU - Clear Channel Comm.',  'CLX - Clorox Co.',  'CMGI - CMG Information Services Inc.',  'CMS - CMS Energy',  'CNET - CNET Inc.',  'COKE - Coca-Cola Bottling Co.',  'KO - Coca-Cola Co',  'CCE - Coca-Cola Enterprises',  'CL - Colgate-Palmolive Co.',  'CMCSK - Comcast Cl\'A\'Spl(non-vtg)',  'CMA - Comerica Inc',  'CMRC - Commerce One Inc.',  'CA - Computer Assoc Intl',  'CSC - Computer Sciences',  'CPWR - Compuware Corp',  'CMVT - Comverse Technology',  'CAG - ConAgra Inc',  'ED - Consolidated Edison',  'CEG - Constellation Energy Group',  'CAL - Continental Airlines',  'CVG - Convergys Corp',  'CBE - Cooper Indus',  'CTB - Cooper Tire & Rubber',  'RKY - Coors (Adolph)Cl\'B\'',  'GLW - Corning Inc.',  'COST - Costco Wholesale',  'COX - Cox Communications Inc.',  'CR - Crane Co',  'CREE - Cree Research Inc.',  'CCK - Crown Cork & Seal',  'CSX - CSX Corp',  'CRGN - Curagen Corp',  'CVS - CVS Corporation',  'CYTO - Cytogen Corp',  'DCX - Daimler Benz & Chrysler',  'DCN - Dana Corp',  'DHR - Danaher Corp',  'DRI - Darden Restaurants',  'DE - Deere & Co.',  'DELL - Dell Computer Corp', 'DPH - Delphi Automotive Systems', 'DAL - Delta Air Lines Inc.',  'DEL - Deltic Timber Corp.',  'DLX - Deluxe Corp',  'DIA - Diamonds Trust',  'DDS - Dillard\'s Inc\'A\'',  'DIS - Disney Walt Co.',  'DVSA - Diversa Corp',  'DG - Dollar General',  'DLTR - Dollar Tree Stores Inc.',  'D - Dominion Resources',  'DCLK - Doubleclick Inc.',  'DOV - Dover Corp',  'DOW - Dow Chemical Co.',  'DJ - Dow Jones Company Inc.',  'DRYR - Dreyers Grand Ice Cream Inc.',  'DTE - DTE Energy',  'DUK - Duke Energy Corporation',  'DNB - Dun & Bradstreet',  'DD - DuPont de Nemours EI & Co.',  'DPMI - DuPont Photomasks Inc.',  'ELNK - Earthlink Network Inc.',  'EMN - Eastman Chemical',  'EK - Eastman Kodak Co.',  'ETN - Eaton Corp',  'EBAY - Ebay Inc.',  'ELON - Echelon Corp.',  'DISH - Echostar Communications',  'ECL - Ecolab Inc',  'EIX - Edison Intl',  'ERTS - Electronic Arts Inc.',  'EDS - Electronic Data Systems',  'EMC - EMC Corporation',  'EMR - Emerson Electric',  'EC - Engelhard Corp',  'ETR - Entergy Corp',  'ENZ - Enzo Biochem Inc.',  'EQR - Equity Residential',  'EQIX - Equinix Inc.',  'ESV - Ensco plc',  'EQT - EQT Corporation',  'ES - Entergy',  'EXC - Exelon Corporation',  'XRX - Xerox Corp',  'F - Ford Motor Co.',  'FTI - FMC Technologies',  'FOE - Ferro Corp',  'FDX - FedEx Corp',  'FNM - Fannie Mae',  'FRE - Freddie Mac',  'FDS - FactSet Research',  'FMC - FMC Corp',  'FNF - Fidelity National Financial',  'FISV - Fiserv Inc.',  'FLEX - Flextronics International',  'FLIR - FLIR Systems Inc.',  'FOSL - Fossil Inc.',  'F - Ford Motor Company',  'FOXA - Fox Entertainment Group',  'GILD - Gilead Sciences',  'GE - General Electric',  'GGP - General Growth Properties',  'GM - General Motors',  'GWW - Grainger (W.W.)',  'GRMN - Garmin Ltd.',  'GDI - GDI Corp.',  'GPC - Genuine Parts Co.',  'GIS - General Mills Inc.',  'GL - Genesee & Wyoming',  'GT - Goodyear Tire & Rubber',  'GIL - Gildan Activewear Inc.',  'GLW - Glass & Glazing',  'HIG - Hartford Financial Svcs',  'HAS - Hasbro Inc',  'HOG - Harley-Davidson Inc.',  'HAR - Harris Corporation',  'HSY - Hershey Foods Corp',  'HIG - Hartford Financial',  'HST - Host Marriott Corp.',  'HUN - Huntsman Corporation',  'HES - Hess Corp',  'HCBK - Hudson City Bancorp Inc.',  'HUM - Humana Inc.',  'IT - I.T.T. Corp',  'HMSY - HMS Holdings Corp',  'IBM - International Business Machines',  'IDXX - IDEXX Laboratories Inc.',  'IGT - International Game Technology',  'INTC - Intel Corporation',  'ICE - Intercontinental Exchange',  'IR - Ingersoll-Rand Co.',  'JCI - Johnson Controls',  'JPM - JPMorgan Chase & Co.',  'JNJ - Johnson & Johnson',  'KO - Coca-Cola Co.',  'KMB - Kimberly-Clark',  'KMX - CarMax Inc.',  'KHC - Kraft Heinz Co.',  'KSS - Kohl\'s Corp',  'K - Kellogg Company',  'KEY - KeyCorp',  'KSS - Kohl\'s Corp',  'KMI - Kinder Morgan Inc.',  'LB - L Brands, Inc.',  'LEN - Lennar Corp',  'LNT - Lincoln National',  'LLY - Eli Lilly and Company',  'LMT - Lockheed Martin',  'LOW - Lowe\'s Cos Inc.',  'LYG - Lloyds Banking Group',  'MAR - Marriott International', 'MCO - Moody\'s Corporation',  'MCHP - Microchip Technology',  'MSI - Motorola Solutions, Inc.',  'MDT - Medtronic, Inc.',  'MDP - Meredith Corp',  'MET - MetLife Inc',  'MGM - MGM Mirage',  'MSFT - Microsoft Corp',  'MMM - 3M Company',  'MU - Micron Technology Inc.',  'NEM - Newmont Mining Corp.',  'NKE - Nike Inc.',  'NFLX - Netflix, Inc.',  'NWL - Newell Rubbermaid',  'NVDA - NVIDIA Corporation',  'NVS - Novartis AG',  'NTRS - Northern Trust',  'NKE - Nike Inc.',  'NWL - Newell Rubbermaid', 'ORCL - Oracle Corp',  'PEP - PepsiCo, Inc.',  'PFE - Pfizer Inc.',  'PM - Philip Morris International',  'PMTC - PMC-Sierra',  'QCOM - Qualcomm',  'RCL - Royal Caribbean Cruises',  'RL - Ralph Lauren Corp', 'TX - Raytheon Technologies',  'SBUX - Starbucks Corporation',  'SHW - Sherwin-Williams Co.',  'SYY - Sysco Corporation',  'TGT - Target Corp.',  'TWTR - Twitter Inc.',  'TXN - Texas Instruments',  'USB - U.S. Bancorp',  'V - Visa Inc.',  'VZ - Verizon Communications',  'WBA - Walgreens Boots Alliance',  'WMT - Walmart Inc.',  'WAT - Waters Corp',  'XRX - Xerox Corp',  'ZM - Zoom Video Communications, Inc.',  'ZTS - Zoetis Inc.'  // Add more symbols as needed
-      ],
+      stockSymbols: [],
     };
   },
+  created() {
+    this.fetchStockSymbols(); // Fetch symbols when component is created
+  },
   methods: {
+    async fetchStockSymbols() {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/src/static/stock_data.txt');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.text(); // Get the text from the response
+        this.stockSymbols = data.split('\n').map(line => line.trim()).filter(line => line); // Split by newline and clean up
+      } catch (error) {
+        console.error('Error fetching stock symbols:', error);
+      }
+    },
     getChartUrl(ticker) {
       return `http://localhost:8080/candlestick_chart.html?ticker=${ticker}&timestamp=${new Date().getTime()}`;
     },
     updateChart() {
-      this.loading = true;
-      console.log(`Fetching data for ticker: ${this.ticker}`);
-      
+    if (this.ticker.trim() === '') {
+      return; // Exit the method if ticker is empty
+    }
+    
+    // Proceed with fetching the chart
+    this.chartUrl = this.getChartUrl(this.ticker);
+    this.loading = true; // Show loading indicator
+
+    // Fetch the chart data
+    console.log(`Fetching data for ticker: ${this.ticker}`);
+
       setTimeout(async () => {
         try {
           const response = await fetch(`http://localhost:5000/generate_chart?ticker=${this.ticker}`);
           const data = await response.json();
 
           console.log("Response data:", data);
-
           if (data.success) {
-            this.chartUrl = this.getChartUrl(this.ticker);
-            this.stockName = data.stockName;
-            this.stockCode = data.stockCode;
-            this.marketCap = data.marketCap;
-            this.percentChange = data.percentChange;
-            this.volume = data.volume;
+          this.chartUrl = this.getChartUrl(this.ticker);
+          this.stockName = data.longName;
+          this.stockCode = data.symbol;
+          this.marketCap = data.marketCap;
+          this.percentChange = data.percentChange;
+          this.volume = data.volume;
+          this.industry = data.industry; 
+          this.sector = data.sector;
+          this.currentPrice = data.currentPrice;
 
-            console.log(`Chart updated for ticker: ${this.ticker}`);
-          } else {
-            console.error('Error with data response:', data.message);
-            this.stockName = 'Error: Invalid ticker';
-            this.stockCode = 'Error';
-            this.marketCap = 'Error';
-            this.percentChange = 'Error';
-            this.volume = 'Error: No volume found';
-          }
+          console.log(`Chart updated for ticker: ${this.ticker}`);
+        } else {
+          console.error('Error with data response:', data.message);
+          this.stockCode = 'Error: Invalid ticker';
+        }
+
         } catch (error) {
           console.error('Error fetching chart data:', error);
-          this.stockName = 'Error';
-          this.stockCode = 'Error';
-          this.marketCap = 'Error';
-          this.percentChange = 'Error';
-          this.volume = 'Error';
         } finally {
           this.loading = false;
         }
@@ -163,7 +178,7 @@ export default {
     containerStyle() {
       return {
         backgroundColor: 'rgba(32, 52, 68, 1.0)',
-        padding: '20px',
+        padding: '30px',
         borderRadius: '10px',
         position: 'relative',
       };
@@ -173,7 +188,7 @@ export default {
 </script>
 
 <style scoped>
-/* Existing styles */
+
 iframe {
   margin: 0;
   padding: 0;
@@ -256,9 +271,10 @@ iframe {
   padding: 10px;
   color: white;
   font-size: 15px;
+  margin-bottom: -15px;
 }
 
-/* Modal styles */
+/* List styles */
 .modal-overlay {
   position: fixed;
   top: 0;
@@ -273,12 +289,13 @@ iframe {
 }
 
 .modal-content {
-  background-color: #fff;
+  background-color: #f9c802;
   padding: 20px;
   border-radius: 8px;
   width: 400px;
   max-width: 90%;
   text-align: center;
+  margin: auto;   /* Center modal */
 }
 
 .modal-content h2 {
